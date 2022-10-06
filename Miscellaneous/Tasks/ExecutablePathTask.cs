93c62fbe8ed815d69a275381fb40fdbc7690c73f
Kokyett <kokyett@users.noreply.github.com>
2022-10-06 19:48:11 +0200

    Miscellaneous: add ExecutablePathTask

---
+++ Miscellaneous/Tasks/ExecutablePathTask.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+namespace Miscellaneous.Tasks {
+    internal class ExecutablePathTask : TaskBase {
+        protected override void ExecuteTask() {
+            Logger.Log($"Assembly    : {Assembly.GetExecutingAssembly().Location}");
+            Logger.Log($"AppDomain   : {AppDomain.CurrentDomain.BaseDirectory}");
+            Logger.Log($"Environment : {Environment.GetCommandLineArgs()[0]}");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -4,4 +4,5 @@ This repository contains test projects.
 
 ## Miscellaneous
 
-This project uses reflection to instanciate classes and run the Execute method.
+This project uses reflection to instanciate classes and run the Execute method. It contains:
+- ExecutablePathTask: it was created to test the est way to get the assembly path (using single file build and/or application installed into UNC folder).
