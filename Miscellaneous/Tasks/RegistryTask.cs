6b84904e3f5abf9368e81a57767b33ee44f685cb
Kokyett <kokyett@users.noreply.github.com>
2022-10-06 20:13:52 +0200

    Miscellaneous: add RegistryTask

---
+++ Miscellaneous/Tasks/RegistryTask.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using Microsoft.Win32;
+
+namespace Miscellaneous.Tasks {
+    #pragma warning disable CA1416 // Validate platform compatibility
+    internal class RegistryTask : TaskBase {
+        private const string URI_SCHEME = "yturi";
+
+        protected override void ExecuteTask() {
+            string[] args = Environment.GetCommandLineArgs();
+            if (args.Length > 1 && args[1].StartsWith($"{URI_SCHEME}:")) {
+                Process process = new();
+                process.StartInfo.FileName = $"https://www.youtube.com/watch?v={args[1].Replace($"{URI_SCHEME}:", "")}";
+                process.StartInfo.WorkingDirectory = new FileInfo(args[0]).Directory?.FullName;
+                process.StartInfo.UseShellExecute = true;
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+                process.Start();
+            } else {
+                if (Registry.ClassesRoot.OpenSubKey(URI_SCHEME) != null) {
+                    Logger.Log($"HKEY_CLASSES_ROOT/{URI_SCHEME} registry key exists");
+                } else {
+                    Logger.Log($"Create HKEY_CLASSES_ROOT/{URI_SCHEME} registry key");
+                    using RegistryKey key = Registry.ClassesRoot.CreateSubKey(URI_SCHEME);
+                    key.SetValue(null, "URL:Open youtube video");
+                    key.SetValue("URL Protocol", "");
+                    using RegistryKey subKeyShell = key.CreateSubKey("shell");
+                    using RegistryKey subKeyOpen = subKeyShell.CreateSubKey("open");
+                    using RegistryKey subKeyCommand = subKeyOpen.CreateSubKey("command");
+                    subKeyCommand.SetValue(null, $"\"{Environment.GetCommandLineArgs()[0].Replace(".dll", ".exe")}\" %1");
+                }
+            }
+        }
+    }
+    #pragma warning restore CA1416 // Validate platform compatibility
+}
--- README.md
+++ README.md
@@ -7,3 +7,7 @@ This repository contains test projects.
 This project uses reflection to instanciate classes and run the Execute method. It contains:
 - ExecutablePathTask: it was created to test the est way to get the assembly path (using single file build and/or application installed into UNC folder).
 - NatStarTask: read the NatStar dictionary. Don't ask what is NatStar.
+- RegistryTask: write in the Windows registry :
+  - to write in the registry, you have to launch the application with administrator privilege;
+  - it adds a yturi URI scheme that can open youtube video;
+  - create a shorcut on your desktop using yturi:eBGIQ7ZuuiU as path or copy paste yturi:eBGIQ7ZuuiU into  your web navigator.
